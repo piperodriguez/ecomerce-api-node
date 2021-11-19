5e406be338ac9652f9b036178276324c5bcf0625
Juan Felipe <vargasjuan367@gmail.com>
2021-11-19 15:22:53 -0500

    Instalacion express y definiendo servidor

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('ejecutando');
+const express = require('express');
+//
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor ejecutandose en express');
+});
+
+app.listen(port, ()=>{
+  console.log('ejecutando en el puerto '+port);
+});
